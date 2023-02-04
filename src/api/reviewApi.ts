82bb09fe50a23fa0a3f3a4d2ec29dd5b456f7565
Dong Le <61934131+dongle47@users.noreply.github.com>
2023-02-04 14:04:10 +0700

    UI review product

--- src/api/reviewApi.ts
+++ src/api/reviewApi.ts
@@ -1,17 +1,17 @@
 import { Review } from 'models/review';
 import { axiosClient } from './axiosClient';
 
-const apiReviews = {
-    postReview: async (params:Review) => {
+const reviewsApi = {
+    postReview: async (params:any) => {
         const res = await axiosClient.post('/reviews/', params)
         return res.data;
     },
 
-    findReviews: async (params:Review) => {
+    findReviews: async (params:any) => {
         const res = await axiosClient.post('/reviews/find', params)
         return res.data;
     },
 
 
 }
-export default apiReviews; 
+export default reviewsApi; 
